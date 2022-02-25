d93c315b630aad3fc9fe7a6a495b245f9574e213
Madjid-Djennane <madjid.djennane@outlook.fr>
2022-02-25 18:53:49 +0100

    clean script

--- script.sh
+++ script.sh
@@ -3,27 +3,16 @@
 git fetch -p
 
 branches_list_str=$(git branch -r --list --sort=committerdate "*release_v*")
-#branches_list_str=$(echo $branches_list_str | xargs -n1 | sort | xargs)
 
 branches_list=(${branches_list_str//\ / })
 echo "release branches >> " $branches_list_str
 
+# current release
 current_release=${branches_list[-1]}
 current_release_name=(${current_release//\// })
 echo "current release name >> " ${current_release_name[-1]}
 
-echo 
-
+# second last release
 previous_release=${branches_list[-2]}
 previous_release_name=(${previous_release//\// })
 echo "previous release >> " ${previous_release_name[-1]}
-
-
-# for branch in $branches_list
-# do
-#     echo $branch
-# done
-
-# my_list='a z t b e c'
-# my_list=$(echo $my_list | xargs -n1 | sort | xargs)
-# echo $my_list
